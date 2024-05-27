f0df1f8be15ea8858b925c19ab21a62b28ab28cc
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-27 15:02:19 -0300

    First files done

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running in the port ${PORT}`);
+})
